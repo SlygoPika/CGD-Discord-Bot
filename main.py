ea15a559d47ac491ff7fed29eac7f6fce93fda1f
SlygoPika <hmorindg25@gmail.com>
2024-01-17 13:54:13 -0500

    command

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from typing import Final
 import os
 from dotenv import load_dotenv
 from discord import Intents, Client, Message
+from discord.ext import commands
 
 load_dotenv()
 TOKEN: Final[str] = os.getenv('BOT_TOKEN')
@@ -13,6 +14,8 @@ intents: Intents = Intents.default()
 intents.message_content = True
 client: Client = Client(intents=intents)
 
+bot = commands.Bot(command_prefix='$', intents=intents)
+
 async def send_message(message: Message, user_message: str) -> None:
     # response: str = get_response(message.content)
     # await message.channel.send(response)
@@ -21,5 +24,12 @@ async def send_message(message: Message, user_message: str) -> None:
         return
     
     await message.channel.send('Hello World')
+
+@bot.command()
+async def test(ctx, arg):
+    await ctx.send(arg)
+    pass
+
+bot.add_command(test)
 
 client.run(TOKEN)
