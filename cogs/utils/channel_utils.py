5a43fb25e8180141a117416dfc99e3cc886e315e
Kevin Luo <91026604+Luo-Kevin@users.noreply.github.com>
2024-01-18 09:49:56 -0500

    Change team title to its prefix

--- cogs/utils/channel_utils.py
+++ cogs/utils/channel_utils.py
@@ -1,9 +1,13 @@
+import cogs.utils.constants as constants
+
+
 def auto_team_channel_naming(existing_teams):
     team_count = 0
-    title = f"Team-{str(team_count).zfill(4)}"
+    title = f"{constants.NEW_TEAM_NAME_PREFIX}{str(team_count).zfill(4)}"
 
     while title in existing_teams:
-        temp_title = f"Team-{str(team_count).zfill(4)}"
+        temp_title = f"{constants.NEW_TEAM_NAME_PREFIX}{
+            str(team_count).zfill(4)}"
         if temp_title not in existing_teams:
             title = temp_title
             return title
--- cogs/utils/constants.py
+++ cogs/utils/constants.py
@@ -1,2 +1,2 @@
 CREATE_TEAM_MESSAGE = "React here to join"
-NEW_TEAM_NAME_PREFIX = "team"
+NEW_TEAM_NAME_PREFIX = "Team-"
