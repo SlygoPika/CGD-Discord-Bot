91a67d428aa6a8fafdf702c352842c539b174971
SlygoPika <hmorindg25@gmail.com>
2024-01-19 12:17:17 -0500

    fixed small bug in channel_utils

--- cogs/utils/channel_utils.py
+++ cogs/utils/channel_utils.py
@@ -6,8 +6,7 @@ def auto_team_channel_naming(existing_teams):
     title = f"{constants.NEW_TEAM_NAME_PREFIX}{str(team_count).zfill(4)}"
 
     while title in existing_teams:
-        temp_title = f"{constants.NEW_TEAM_NAME_PREFIX}{
-            str(team_count).zfill(4)}"
+        temp_title = f"{constants.NEW_TEAM_NAME_PREFIX}{str(team_count).zfill(4)}"
         if temp_title not in existing_teams:
             title = temp_title
             return title
